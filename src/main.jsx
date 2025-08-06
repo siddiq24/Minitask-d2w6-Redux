83031ea86f09c6e6ed3154ee59d913b512d91d50
siddiq24 <siddiqrm24@gmail.com>
2025-08-07 05:36:01 +0700

    feat(main): add provider by redux for using store in all component

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Provider } from 'react-redux'
+import { store } from './redux/store'
+import Router from './Router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Provider store={store}>
+      <Router/>
+    </Provider>
   </StrictMode>,
 )
